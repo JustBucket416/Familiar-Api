dbe20a04eb16196615ea762a2726566a446ea947
JustBucket416 <26853613+justbucket416@users.noreply.github.com>
2025-04-07 14:38:00 +0900

    added model loading

--- src/justbucket/familiar/extension/ExtensionInitializer.kt
+++ src/justbucket/familiar/extension/ExtensionInitializer.kt
@@ -2,15 +2,20 @@ package justbucket.familiar.extension
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import justbucket.familiar.extension.model.ListModel
 
 /**
  * @author JustBucket on 03.04.2025
  */
-abstract class ExtensionInitializer {
+abstract class ExtensionInitializer(val extensionName: String) {
 
     lateinit var context: Context
 
     abstract val isInitialized: Boolean
 
-    abstract val extensionInitializerScreen: @Composable () -> Unit
+    abstract val extensionInitializerScreen: @Composable (() -> Unit) -> Unit
+
+    open fun getPreloadedModels(): List<Pair<ListModel, List<String>>> {
+        return emptyList()
+    }
 }
